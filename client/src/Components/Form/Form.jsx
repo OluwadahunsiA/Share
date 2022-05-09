0a0d7e978c1eba34d18f27e208263e040db60f19
OluwadahunsiA <tommydate0@gmail.com>
2022-05-10 00:59:24 +0300

    fix input image

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import { Typography } from '@mui/material';
 import Paper from '@mui/material/Paper';
@@ -9,14 +9,33 @@ import CloseIcon from '@mui/icons-material/Close';
 import { IconButton, Button } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { closeModal } from '../../Actions/movieActions';
+import FileBase from 'react-file-base64';
 
 // eslint-disable-next-line react/prop-types
 const Form = ({ open }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const [movie, setMovie] = useState({
+    title: '',
+    description: '',
+    tags: '',
+    file: '',
+  });
 
   const handleFormClose = () => {
     dispatch(closeModal());
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(movie.file[0]);
+    console.log(movie);
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name !== 'file') {
+      setMovie({ ...movie, [e.target.name]: e.target.value });
+    }
   };
   return (
     <Modal open={open}>
@@ -41,24 +60,45 @@ const Form = ({ open }) => {
             <TextField
               className={classes.formField}
               label='Title'
+              name='title'
+              value={movie.title}
               margin='normal'
-              focused={false}
+              required
+              focused
+              onChange={handleChange}
             ></TextField>
             <TextField
               className={classes.formField}
               label='Description'
+              name='description'
+              value={movie.description}
               margin='normal'
               focused={false}
+              required
+              onChange={handleChange}
             ></TextField>
             <TextField
               className={classes.formField}
+              value={movie.tags}
               label='Tags'
+              name='tags'
               margin='normal'
               focused={false}
+              required
+              onChange={handleChange}
             ></TextField>
-            <input className={classes.inputField} type='file'></input>
 
-            <Button variant='contained' color='inherit'>
+            <FileBase
+              type='file'
+              multiple={false}
+              onDone={({ base64 }) => setMovie({ ...movie, file: base64 })}
+            />
+
+            <Button
+              variant='contained'
+              color='inherit'
+              onClick={handleFormSubmit}
+            >
               Add
             </Button>
           </Paper>
