93108406e11c642ae306f4a335777910ab6372d8
OluwadahunsiA <tommydate0@gmail.com>
2022-05-17 00:17:35 +0300

    generate user token

--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -1,13 +1,53 @@
 const User = require('../Model/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-exports.signUpUser = (req, res) => {
-  console.log(req.body);
+exports.signUpUser = async (req, res) => {
+  const { email, password, username, confirmPassword } = req.body;
   try {
-    res.status(200).json({
-      status: 'success',
-      message: 'success',
-    });
+    // check if the user does not already exist in the dB.
+
+    const user = await User.findOne({ email });
+
+    if (!(password === confirmPassword)) {
+      res.json({
+        status: 'fail',
+        message: 'passwords do not match',
+      });
+    }
+
+    if (!user) {
+      // hash the password of the user and prepare for storing in DB.
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const newUser = await User.create({
+        email,
+        username,
+        password: hashedPassword,
+        confirmPassword: hashedPassword,
+      });
+
+      // sign a token and send it to the new user
+
+      const token = jwt.sign(
+        { email: newUser.email, id: newUser._id },
+        process.env.SECRET,
+        { expiresIn: '1h' }
+      );
+
+      res.status(200).json({
+        status: 'success',
+        data: token,
+      });
+    } else {
+      return res.json({
+        status: 'fail',
+        message: 'This user already exists  ',
+      });
+    }
   } catch (err) {
+    console.log(err.message);
     res.json({
       status: 'fail',
       message: err.message,
